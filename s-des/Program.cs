26faad75f522706933145eef9b209661d2e8a9f0
abtinokhovat <abtinokhovat@gmail.com>
2023-01-22 20:20:22 +0330

    refactor: api files

--- s-des/Program.cs
+++ s-des/Program.cs
@@ -5,14 +5,14 @@ using Formatting = Newtonsoft.Json.Formatting;
 //int[] plainBits = {1,0,0,1,0,1,1,0};
 
 Console.WriteLine("Insert 8-bit Plain Text");
-//var plain = Console.ReadLine();
-var plain = "10010110";
+var plain = Console.ReadLine();
+//var plain = "10010110";
 if(plain!.Length != 8) throw new Exception("Key must be 8 bits long");
 Console.WriteLine("\n");
 
 Console.WriteLine("Insert 10-bit Key");
-//var key = Console.ReadLine();
-var key = "1001000100";
+var key = Console.ReadLine();
+//var key = "1001000100";
 if(key!.Length != 10) throw new Exception("Key must be 10 bits long");
 Console.WriteLine("\n");
 
