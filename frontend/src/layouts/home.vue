c69ffeef3cc3b55ff1adb78f95932b25ee18feac
abtinokhovat <abtinokhovat@gmail.com>
2023-01-22 20:22:06 +0330

    init frontend

---
+++ frontend/src/layouts/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+
+
+    <TheFooter />
+    <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm">
+      Abtin Okhovat
+    </div>
+  </main>
+</template>
