4703230c474466578b51aa1b23cc85f4174ae47f
abtinokhovat <abtinokhovat@gmail.com>
2023-04-12 22:47:48 +0330

    docs: completed README.md

--- s-des-typescript/src/playground.ts
+++ s-des-typescript/src/playground.ts
@@ -1,10 +1,14 @@
 import Cryptography from "./class/Cryptography";
-import { BinaryUtil } from "./class/BinaryUtil";
 
-const string = "google";
+const string = "Hello,World but encrypted with s-des";
 const secret = [1, 0, 0, 0, 1, 0, 1, 1, 1, 0];
 
 const keys = Cryptography.GenerateKey(secret);
 const cipher = Cryptography.Encryption(string, keys);
-console.log(cipher.toString());
-console.log(BinaryUtil.toAscii(cipher));
+
+console.log(`Cipher Bits:${cipher.toString()}`);
+console.log(`Cipher Ascii:${cipher.toAscii()}`);
+
+const plain = Cryptography.Decryption(cipher, keys);
+
+console.log(`Plain Text Decrypted:${plain}`);
