ef7e04779e103be7a4e169a64112cc682e3419ed
abtinokhovat <abtinokhovat@gmail.com>
2023-04-12 22:50:42 +0330

    lib: added new classes to library

--- s-des-typescript/src/index.ts
+++ s-des-typescript/src/index.ts
@@ -2,5 +2,8 @@ import BitBuffer from "./class/BitBuffer";
 import KeyGenerator from "./class/KeyGenerator";
 import Mapper from "./class/Mapper";
 import Transformer from "./class/Transformer";
+import BitBufferArray from "./class/BitBufferArray";
+import Cryptography from "./class/Cryptography";
 
-export { BitBuffer, KeyGenerator, Mapper, Transformer };
+export { BitBufferArray, BitBuffer, KeyGenerator, Mapper, Transformer };
+export default Cryptography;
